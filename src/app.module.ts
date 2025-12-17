cb45af735c1ebbdd0aebf8ce6859f7cf14e66850
Dev-Waqas232 <devwaqas232@gmail.com>
2025-12-17 12:40:44 +0500

    cleaned up tutorial stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BullModule } from '@nestjs/bullmq';
-import { VideosModule } from './videos/videos.module';
 
 @Module({
   imports: [
@@ -14,7 +13,6 @@ import { VideosModule } from './videos/videos.module';
         removeOnFail: 1000,
       },
     }),
-    VideosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
